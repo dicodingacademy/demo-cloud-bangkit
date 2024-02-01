6fafe1879c1647d5355d26d023c998bcfddd2c29
ridhaginanjar <ridhaginanjar7@gmail.com>
2024-02-01 18:08:35 +0700

    push code to GitHub

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# demo-cloud-bangkit
+# Demo ILT 2
+## Journey into the Cloud: Google Cloud's Compute, Storage, and Database
+
+Steps:
+1. Clone this repo.
+2. Run this syntax using gcloud:
+```
+gcloud run deploy <SERVICES> --port 5000 --source .
+```
---
+++ server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5000,
+    host: '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
